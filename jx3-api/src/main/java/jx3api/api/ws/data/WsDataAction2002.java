acbd4f9b9de4e9d85883e1b70bb6b057c4b247f4
field <z_w_field@163.com>
2024-09-27 17:15:51 +0800

    fix: 修复订阅

--- jx3-api/src/main/java/jx3api/api/ws/data/WsDataAction2002.java
+++ jx3-api/src/main/java/jx3api/api/ws/data/WsDataAction2002.java
@@ -20,7 +20,7 @@ import lombok.ToString;
 public class WsDataAction2002 extends BaseWsData {
 
 
-    @JsonProperty("type")
+    @JsonProperty("class")
     private String type;
     @JsonProperty("title")
     private String title;
